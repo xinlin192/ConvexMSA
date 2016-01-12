0bd49830b95a5eb7f03b6c218fccfbeb6480b6f1
jimmylin192 <linxin@gmail.com>
2016-01-11 23:41:16 -0600

    write a new function: pair2co.py

---
+++ codebases/util/pair2co.py
@@ -0,0 +1,60 @@
+import sys
+from util import parseSequences
+
+def usage():
+    usage_info = '''This program converts .pair file to .co file for multiple sequence alignment.
+    python pair2co.py [data_file]
+It is assumed that all sequences in the input file are of the same length
+Please use Python 2.7 to run this program.'''
+    print usage_info
+
+def convertPair2CO (allModelSeqs, allDataSeqs):
+    numSeqs = len(allModelSeqs)
+    allCOSeqs = ["" for i in range(numSeqs)]
+    pos = [0 for i in range(numSeqs)]
+    while True:
+        insertions_ids = {}
+        for i in range(numSeqs):
+            if pos[i] >= len(allModelSeqs[i]): continue
+            if (allModelSeqs[i][pos[i]] == '-'):
+                insertions_ids[i] = True
+        if bool(insertions_ids):
+            for i in range(numSeqs):
+                if i in insertions_ids:
+                    allCOSeqs[i] += allDataSeqs[i][pos[i]]
+                    pos[i] += 1
+                else:
+                    allCOSeqs[i] += '-'
+        else:
+            for i in range(numSeqs):
+                allCOSeqs[i] += allDataSeqs[i][pos[i]]
+                pos[i] += 1
+        terminated = True
+        for i in range(numSeqs):
+            if (pos[i] != len(allModelSeqs[i])):
+                terminated = False
+                break
+        if terminated: break
+    
+    return allCOSeqs
+
+def main():
+    argn = len(sys.argv)
+    if argn != 2:
+        usage()
+        sys.exit()
+    seqfile = sys.argv[1]
+    allSeqs = parseSequences(seqfile)
+    numSeqs = len(allSeqs)
+    allModelSeqs = []
+    allDataSeqs = []
+    for i in range(0, numSeqs,2):
+        allModelSeqs.append(allSeqs[i])
+    for i in range(1, numSeqs,2):
+        allDataSeqs.append(allSeqs[i])
+    allCOSeqs = convertPair2CO (allModelSeqs, allDataSeqs)
+    for seq in allCOSeqs:
+        print " ", seq
+    
+if __name__ == "__main__":
+    main()
