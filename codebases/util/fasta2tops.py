a30d59c435fa31b28aec381e16d2efbe09a503f1
jimmylin192 <linxin@gmail.com>
2016-01-16 21:02:54 -0600

    fasta2tops

--- codebases/util/fasta2tops.py
+++ codebases/util/fasta2tops.py
@@ -1,11 +1,29 @@
 import sys
 
+def fasta2tops(allNames, allSeqs):
+    nSeqs = len(allSeqs)
+    for i in range(nSeqs):
+        tmp_str = allNames[i].replace(" ", "_") + ": "
+        for j in range(len(allSeqs[i])):
+            tmp_str += allSeqs[i][j] + " "
+        print tmp_str
+
 def main():
     if len(sys.argv) < 2:
         usage()
         return
-
     fname = sys.argv[1]
+
+    allNames = []
+    allSequences = []
+    i = 0
+    fp = open(fname)
+    for line in fp:
+        if i % 2 == 0: allNames.append(line.strip("\n"))
+        else: allSequences.append(line.strip("\n"))
+        i += 1
+    fasta2tops(allNames, allSequences)
+    fp.close()
     
 
 if __name__ == '__main__':
